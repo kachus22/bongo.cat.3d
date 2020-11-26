bf1e61197785c4b4745c110b415c797a65efbc14
Enrique Abrahm García Torres <garcia.enrique.torres@gmail.com>
2020-11-25 18:50:53 -0600

    Improve lighting and scene atmosphere

--- js/index.js
+++ js/index.js
@@ -1,58 +1,128 @@
 // Detect if WebGL is supported
 if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
+window.addEventListener( 'resize', onWindowResize, false );
+
 var statsEnabled = true, container, stats;
-var camera, scene, renderer;
+var camera, controls, renderer, scene;
 var mouseX = 0, mouseY = 0;
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
-var controls;
 const CAT_COLOR = 0xFFFFFF;
 const PAWS_COLOR = 0xFE9BA1;
-var cat, body, ears, arms, eyes, smile, bongos, legs;
+const loader = new THREE.TextureLoader();
+loader.setCrossOrigin('');
+var cat, body, ears, arms, eyes, smile, bongos, legs, table, house;
+
+const ballPosition = new THREE.Vector3( 0, - 45, 0 );
+const ballSize = 60; //40
+
+let sphere;
+let object;
 
 init();
 animate();
 
-// Initialize the scene, objects, and so on.
 function init() {
   // CONTAINER
   container = document.createElement( 'div' );
   document.body.appendChild( container );
 
-  // CAMERA
-  camera = new THREE.PerspectiveCamera( 45,
-    window.innerWidth / window.innerHeight, .1, 1000 );
-  camera.position.x = 14;
-  camera.position.y = 12;
-  camera.position.z = 40;
-  // camera.position.x = 0;
-  // camera.position.y = 0;
-  // camera.position.z = 38;
+  // SCENE
+  // Create a new Three.js scene
   scene = new THREE.Scene();
+  scene.background = new THREE.Color( 0xd8b365 );
+  scene.fog = new THREE.Fog( 0xd8b365, 500, 10000 );
 
-  buildCat();
-  buildTable();
-  buildBongos();
+  // CAMERA
+  // Create a camera so we can view the scene
+  camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 1, 10000 );
+  camera.position.set( 800, 350, 1500 );
 
-  // POINTLIGHT
-  let pointLight = new THREE.PointLight( 0xFFFFFF );
-  pointLight.position.z = 1000;
-  // pointLight.position.y = -200; 
-  scene.add(pointLight);
-  pointLight = new THREE.PointLight( 0xFFFFFF );
-  pointLight.position.z = -1000;
-  pointLight.position.y = -200; 
-  scene.add(pointLight);
-  pointLight = new THREE.PointLight( 0xFFFFFF );
-  pointLight.position.z = 0;
-  pointLight.position.y = 100; 
-  pointLight.position.x = 50; 
-  scene.add(pointLight);
+  // LIGHTS
+  const ambientLight = new THREE.AmbientLight( 0x666666 );
+  // ambientLight.intensity = 0.2;
+  scene.add( ambientLight );
+
+  const light = new THREE.DirectionalLight( 0xdfebff, 1 );
+  light.position.set( 60, 150, -120 );
+  light.position.multiplyScalar( 1.3 );
+  light.castShadow = true;
+  // light.intensity = 0.9;
+  light.shadow.mapSize.width = 1024;
+  light.shadow.mapSize.height = 1024;
+
+  const d = 300;
+  light.shadow.camera.left = - d;
+  light.shadow.camera.right = d;
+  light.shadow.camera.top = d;
+  light.shadow.camera.bottom = - d;
+  light.shadow.camera.far = 1000;
+  scene.add( light );
+
+  const light2 = new THREE.DirectionalLight( 0xff0000, 1 );
+  light2.position.set( 20, 0, -100 );
+  light2.position.multiplyScalar( 1.3 );
+  scene.add( light2 );
+
+  const light3 = new THREE.DirectionalLight( 0xff0000, 1 );
+  light3.position.set( 60, 150, -120 );
+  light3.position.multiplyScalar( 1.3 );
+  light3.castShadow = true;
+  light3.intensity = 1.4;
+  light3.shadow.mapSize.width = 1024;
+  light3.shadow.mapSize.height = 1024;
+
+  light3.shadow.camera.left = - d;
+  light3.shadow.camera.right = d;
+  light3.shadow.camera.top = d;
+  light3.shadow.camera.bottom = - d;
+  light3.shadow.camera.far = 1000;
+  scene.add( light3 );
+
+  const light4 = new THREE.DirectionalLight( 0xff0000, 1 );
+  light4.position.set( 80, 80, -80 );
+  light4.position.multiplyScalar( 2.3 );
+  light4.castShadow = true;
+  light4.intensity = 1.4;
+  light4.shadow.mapSize.width = 1024;
+  light4.shadow.mapSize.height = 1024;
+
+  light4.shadow.camera.left = - d;
+  light4.shadow.camera.right = d;
+  light4.shadow.camera.top = d;
+  light4.shadow.camera.bottom = - d;
+  light4.shadow.camera.far = 1000;
+  scene.add( light4 );
+
+  // SPHERE
+  const ballGeo = new THREE.SphereBufferGeometry( ballSize, 32, 16 );
+  const ballMaterial = new THREE.MeshLambertMaterial();
+
+  sphere = new THREE.Mesh( ballGeo, ballMaterial );
+  sphere.castShadow = true;
+  sphere.receiveShadow = true;
+  sphere.visible = false;
+  scene.add( sphere );
+
+  setPositions();
 
   // RENDERER
-  renderer = new THREE.WebGLRenderer({ antialias: true });
+  // Create the Three.js renderer and attach it to our canvas
+  renderer = new THREE.WebGLRenderer( { antialias: true } );
+  // Set the viewport size
   renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  // Turn on shadows
+  renderer.shadowMap.enabled = true;
+  // Attach the Three.js renderer
   container.appendChild( renderer.domElement );
 
   // MONITOR
@@ -68,9 +138,37 @@ function init() {
   // CONTROLS
   controls = new THREE.OrbitControls( camera, renderer.domElement );
   controls.target.set( 0, 0.5, 0 );
-  controls.update();
   controls.enablePan = false;
   controls.enableDamping = false;
+  controls.maxPolarAngle = Math.PI * 0.5;
+  controls.minDistance = 800;
+  controls.maxDistance = 4000;
+  controls.update();
+}
+
+function scaleObject(object) {
+  object.scale.x = 30
+  object.scale.y = 30
+  object.scale.z = 30
+}
+
+function setPositions() {
+  buildGround();
+  buildCat();
+  buildTable();
+  buildBongos();
+
+  scaleObject(table);
+  scaleObject(cat);
+  scaleObject(bongos);
+  scene.add(table);
+  scene.add(cat);
+  scene.add(bongos);
+  cat.position.y = -40;
+  bongos.position.y = -40;
+  table.position.y = -140;
+  table.position.z = 310;
+  buildTrees();
 }
 
 // For future usage, let the animations work.
@@ -82,6 +180,7 @@ function animate() {
 
 // Render the scene
 function render() {
+  sphere.position.copy( ballPosition );
   // For future reference, to move the camera position based on the mouse position.
   // camera.position.x += ( mouseX - camera.position.x ) * .005;
   // camera.position.y += ( - mouseY - camera.position.y ) * .005;
@@ -91,6 +190,7 @@ function render() {
   arms[0].rotation.x = Math.PI * 0.5;
   renderer.render( scene, camera );
 }
+      
 
 function buildCat() { 
   cat = new THREE.Object3D();
@@ -100,7 +200,6 @@ function buildCat() {
   buildEars();
   buildEyes();
   buildSmile();
-  scene.add(cat);
 }
 
 function buildBody() {
@@ -121,6 +220,8 @@ function buildBody() {
   const material = new THREE.MeshLambertMaterial( { color: CAT_COLOR } );
   body = new THREE.Mesh( geometry, material );
   body.position.set(0, 0, 0);
+  body.receiveShadow = true;
+  body.castShadow = true;
   cat.add(body);
 }
 
@@ -131,6 +232,8 @@ function buildEars() {
   ears.push( new THREE.Mesh( geometry, material ) );
   ears[0].position.set(-2.7, 6.1, 0);
   ears[0].rotation.z = 0.4;
+  ears[0].receiveShadow = true;
+  ears[0].castShadow = true;
   ears.push( ears[0].clone() )
   ears[1].position.set(2.7, 6.1, 0);
   ears[1].rotation.z = -0.42;
@@ -142,24 +245,30 @@ function buildLimb() {
   const geometry = new THREE.CylinderGeometry( 1.2, 1.6, 4, 32 );
   const material = new THREE.MeshLambertMaterial( { color: CAT_COLOR } );
   const sleeve = new THREE.Mesh( geometry, material );
+  sleeve.receiveShadow = true;
+  sleeve.castShadow = true;
   return sleeve;
 }
 
 function buildLimbSmallEnd() {
   const geometry = new THREE.SphereGeometry( 1.2, 32, 32, 0, Math.PI );
-  const material = new THREE.MeshLambertMaterial( { color: CAT_COLOR, side: THREE.DoubleSide } );
+  const material = new THREE.MeshLambertMaterial( { color: CAT_COLOR } );
   const hand = new THREE.Mesh( geometry, material );
   hand.rotation.x = Math.PI * 1.5  ;
   hand.position.y = 2;
+  hand.receiveShadow = true;
+  hand.castShadow = true;
   return hand;
 }
 
 function buildLimbBigEnd() {
   const geometry = new THREE.SphereGeometry( 1.6, 32, 32, 0, Math.PI );
-  const material = new THREE.MeshLambertMaterial( { color: CAT_COLOR, side: THREE.DoubleSide } );
+  const material = new THREE.MeshLambertMaterial( { color: CAT_COLOR } );
   const elbow = new THREE.Mesh( geometry, material );
   elbow.rotation.x = Math.PI * 0.5  ;
   elbow.position.y = -2;
+  elbow.receiveShadow = true;
+  elbow.castShadow = true;
   return elbow;
 }
 
@@ -171,6 +280,8 @@ function buildPaws() {
   sphere.position.set(0, 2, 0.8);
   sphere.scale.x = 0.9;
   paws[0].add( sphere );
+  paws[0].receiveShadow = true;
+  paws[0].castShadow = true;
   geometry = new THREE.SphereGeometry( 0.35, 32, 32, 0, Math.PI );
   sphere = new THREE.Mesh( geometry, material );
   sphere.position.set(0, 2.7, 0.7);
@@ -201,6 +312,8 @@ function buildPawsBottom() {
   sphere.scale.x = 1.1;
   sphere.rotation.x = Math.PI*0.5;
   paws[0].add( sphere );
+  paws[0].receiveShadow = true;
+  paws[0].castShadow = true;
   geometry = new THREE.SphereGeometry( 0.65, 32, 32, 0, Math.PI );
   sphere = new THREE.Mesh( geometry, material );
   sphere.position.set(0, -2.90, 0.42);
@@ -318,12 +431,32 @@ function buildSmile() {
 }
 
 function buildTable() {
-  const geometry = new THREE.BoxGeometry(20, 10, 0.5);
-  const material = new THREE.MeshPhongMaterial({ color: 'purple' });
-  const table = new THREE.Mesh(geometry, material);
-  table.position.set( 0, -3.2, 11);
+  const geometry = new THREE.BoxGeometry(20, 10, 0.8);
+  const texture = loader.load('img/wood-texture.jpg');
+  const material = new THREE.MeshPhongMaterial( { map: texture, bumpMap: texture, bumpScale: 1 } );
+  table = new THREE.Mesh(geometry, material);
+  table.position.set( 0, -3.36, 11);
   table.rotation.x = Math.PI * 0.5;
-  scene.add(table);
+  table.castShadow = true;
+  table.receiveShadow = true;
+  buildTableLegs();
+}
+
+function buildTableLegs() {
+  const geometry = new THREE.BoxGeometry(10, 3.2, 1);
+  const texture = loader.load('img/wood-texture.jpg');
+  const material = new THREE.MeshPhongMaterial( { map: texture, bumpMap: texture, bumpScale: 1 } );
+  // const material = new THREE.MeshPhongMaterial({ color: 'purple' });
+  const leg = new THREE.Mesh(geometry, material);
+  leg.position.set( 9, 0, 2);
+  leg.rotation.x = Math.PI * 0.5;
+  leg.rotation.y = Math.PI * 0.5;
+  leg.castShadow = true;
+  leg.receiveShadow = true;
+  const leg1 = leg.clone();
+  leg1.position.x = -9;
+  table.add(leg);
+  table.add(leg1);
 }
 
 function buildBongos() {
@@ -332,21 +465,127 @@ function buildBongos() {
   const y = -1.45;
   const z = 8;
   const geometry = new THREE.CylinderGeometry( 2, 1.6, 3, 32 );
-  const material = new THREE.MeshLambertMaterial( { color: 'green' } );
-  const bongo1 = new THREE.Mesh( geometry, material );
+  const topTexture = loader.load('img/bongo-texture-top.png');
+  topTexture.center = new THREE.Vector2(0.5, 0.5);
+  topTexture.rotation = -Math.PI * 0.5;
+  const sidesTexture = loader.load('img/bongo-texture-sides.jpg');
+  const materials = [
+    new THREE.MeshPhongMaterial( { map: sidesTexture, bumpMap: sidesTexture, bumpScale: 1 } ),
+    new THREE.MeshPhongMaterial( { map: topTexture, bumpMap: topTexture, bumpScale: 1  } ),
+    new THREE.MeshPhongMaterial({map: loader.load('img/wood-texture.jpg')})
+  ]
+  const bongo1 = new THREE.Mesh( geometry, materials );
   bongo1.position.set( x, y, z);
+  bongo1.receiveShadow = true;
+  bongo1.castShadow = true;
   const bongo2 = bongo1.clone();
   bongo2.position.x = -x;
   bongos.add( bongo1 );
   bongos.add( bongo2 );
   buildBongosBridge();
-  scene.add( bongos );
 }
 
 function buildBongosBridge() {
   const geometry = new THREE.BoxGeometry(4, 1, 1);
-  const material = new THREE.MeshPhongMaterial({ color: 'green' });
+  const texture = loader.load('img/bongo-texture-connection.png');
+  const material = new THREE.MeshPhongMaterial({ map: texture, bumpMap: texture, bumpScale: 1, side: THREE.DoubleSide });
   const bridge = new THREE.Mesh(geometry, material);
   bridge.position.set( 0, -1.3, 8);
   bongos.add( bridge );
+}
+
+function buildGround() {
+  const texture = loader.load( 'img/grass-texture.jpg' );
+  texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+  texture.repeat.set( 25, 25 );
+  texture.anisotropy = 16;
+  texture.encoding = THREE.sRGBEncoding;
+  const material = new THREE.MeshLambertMaterial( { map: texture } );
+  let ground = new THREE.Mesh( new THREE.PlaneBufferGeometry( 20000, 20000 ), material );
+  ground.position.y = - 250;
+  ground.rotation.x = - Math.PI / 2;
+  ground.receiveShadow = true;
+  scene.add( ground );
+}
+
+function buildTrees() {
+  const treesToAdd = Math.floor(Math.random() * 8) + 4;
+  // const treesToAdd = 1;
+  for( let i = 0; i <= treesToAdd; i++) {
+    let newTree = buildTree();
+    let randomX = -1 * (Math.floor(Math.random() * 2000) + 800);
+    let randomZ = Math.floor(Math.random() * 4000) + 700 * randomSign();
+    newTree.position.set(randomX, 0, randomZ);
+    scene.add(newTree);
+  }
+
+  for( let i = 0; i <= treesToAdd; i++) {
+    let newTree = buildTree();
+    let randomX = -1 * (Math.floor(Math.random() * 2000) + 800);
+    let randomZ = (Math.floor(Math.random() * 4000) + 1200) * -1;
+    newTree.position.set(randomX, 0, randomZ);
+    scene.add(newTree);
+  }
+
+  for( let i = 0; i <= treesToAdd/2; i++) {
+    let newTree = buildTree();
+    let randomX = (Math.floor(Math.random() * 1000) + 400);
+    let randomZ = (Math.floor(Math.random() * 2000) + 1000) * -1;
+    newTree.position.set(randomX, 0, randomZ);
+    scene.add(newTree);
+  }
+}
+
+function buildTree() {
+  const geometry = new THREE.BoxGeometry(220, 800, 180);
+  const texture = loader.load('img/tree-texture.jpg');
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.wrapT = THREE.RepeatWrapping;
+  texture.repeat.set( 1, 4 );
+  const material = new THREE.MeshPhongMaterial( { map: texture } );
+  let tree = new THREE.Mesh(geometry, material);
+  let leaves = buildTreeLeaves();
+  tree.add(leaves);
+  return tree;
+}
+
+function buildTreeLeaves() {
+  const geometry = new THREE.BoxGeometry(520, 500, 380);
+  const texture = loader.load('img/leave-texture.jpg');
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.wrapT = THREE.RepeatWrapping;
+  texture.repeat.set( 1, 1 );
+  const material = new THREE.MeshPhongMaterial( { map: texture, bumpMap: texture, bumpScale: 1 } );
+  let leaves = new THREE.Mesh(geometry, material);
+  leaves.position.y = 380;
+  leaves.castShadow = true;
+  leaves.receiveShadow = true;
+  const leavesToAdd = Math.floor(Math.random() * 8) + 2;
+  for( let i = 0; i <= leavesToAdd; i++) {
+    leaves.add(buildTreeLeavesRandom());
+  }
+  return leaves;
+}
+
+function buildTreeLeavesRandom() {
+  let randomX = Math.floor(Math.random() * 300) + 100;
+  let randomY = Math.floor(Math.random() * 400) + 250;
+  let randomZ = Math.floor(Math.random() * 400) + 200;
+  const geometry = new THREE.BoxGeometry(randomX, randomY, randomZ);
+  const texture = loader.load('img/leave-texture.jpg');
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.wrapT = THREE.RepeatWrapping;
+  texture.repeat.set( 1, 1 );
+  const material = new THREE.MeshPhongMaterial( { map: texture, bumpMap: texture, bumpScale: 1 } );
+  let leaves = new THREE.Mesh(geometry, material);
+  leaves.position.y = Math.floor(Math.random() * 260) * randomSign();
+  leaves.position.x = Math.floor(Math.random() * 260) * randomSign();
+  leaves.position.z = Math.floor(Math.random() * 180) * randomSign();
+  leaves.castShadow = true;
+  leaves.receiveShadow = true;
+  return leaves;
+}
+
+function randomSign() {
+  return -1 + Math.round(Math.random()) * 2;
 }
